933caba7f6f165be84ea86fb84cacab41d1171e6
[b2210660] <[b2210660@photon.chitose.ac.jp]>
2022-11-01 21:17:46 +0900

    課題２

---
+++ prmn2022a/Prmn2022/src/lecture03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lecture03 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("input age");
+        int age = input.nextInt();
+
+        if(age<20 && 0<=age){
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        }else if(20<=age){
+            System.out.println("I am " + age + " years old so I can drink liquor.");
+        }else{
+            System.out.println("error");
+        }
+    }
+}
