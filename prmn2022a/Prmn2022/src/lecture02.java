a549061f5abcd22f687606e04aefbb1e929c1229
[b2210660] <[b2210660@photon.chitose.ac.jp]>
2022-11-01 21:16:31 +0900

    課題２

---
+++ prmn2022a/Prmn2022/src/lecture02.java
@@ -0,0 +1,7 @@
+public class lecture02 {
+    public static void main (String[] args){
+        int studentNumber = 2210660;
+        System.out.println("B" + studentNumber + " Toma Kadono");
+
+    }
+}
