972ee9612931f829fb80a9fe8f2ed464b99f1e27
[b2210660] <[b2210660@photon.chitose.ac.jp]>
2022-11-12 15:00:33 +0900

    課題３－１

---
+++ prmn2022a/Prmn2022/src/lecture03/Exercise3_1.java
@@ -0,0 +1,13 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("任意の文字列を入力してください");
+        String a = scanner.next();
+        System.out.println(a);
+
+    }
+}
